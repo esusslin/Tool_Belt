17747713679f0f462c7740c4d702559f7084833b
Emmet Susslin <esusslin@gmail.com>
2016-07-26 12:18:28 -0700

    lol

--- Tool_Belt/Tool_Belt/ViewController.swift
+++ Tool_Belt/Tool_Belt/ViewController.swift
@@ -11,78 +11,55 @@ import UIKit
 class ViewController: UIViewController {
     
     var backendless = Backendless.sharedInstance()
-//    var currentUser = BackendlessUser?
+   
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        registerUser()
+        
+//        findContactByName()
+//        registerUser()
+        
     }
     
-    func registerUser() {
-        
+//    func registerUser() {
 //        Types.tryblock({ () -> Void in
 //            let user : BackendlessUser = BackendlessUser()
-//            user.email = "emmet@gmail.com"
+//            user.email = "esusslin@fuckyou.com"
 //            user.password = "password"
 //            
-////            let hammer = Tool()
-////            hammer.title = "hammer"
-////            hammer.make = "binford"
-////            hammer.toolDescription = "solid fucking hammer"
-////            hammer.ownerId = user.objectId
-////            
-////            let drill = Tool()
-////            drill.title = "drill"
-////            drill.make = "binford"
-////            drill.toolDescription = "solid fucking drill"
-////            drill.ownerId = user.objectId
-//  
+//            let hammer = Tool()
+//            hammer.ownerId = user.objectId
+//            hammer.make = "binford"
+//            hammer.title = "hammer"
+//            hammer.toolDescription = "solid damn hammer"
+//            
+//            user.setProperty("tools", object: hammer)
 //            
 //            self.backendless.userService.registering(user)
-//            print(user.objectId)
-//            var currentUser = self.backendless.userService.currentUser
 //            print("user registered");
 //        },
-//                catchblock: { (exception) -> Void in
+//                       catchblock: { (exception) -> Void in
 //                        print("Server reported an error: \(exception as! Fault)")
-//                    
-//                    
-//        })
-        
-    }
-
-    @IBAction func buttonPressed(sender: AnyObject) {
-        
-        var currentUser = backendless.userService.currentUser
-//        print(currentUser.objectId)
-        
-        saveNewTool()
-    }
-
-    func saveNewTool() {
-        
-        let hammer = Tool()
-        hammer.title = "hammer"
-        hammer.make = "binford"
-        hammer.toolDescription = "solid fucking hammer"
-        hammer.ownerId = "BBD41923-F796-A7F9-FF93-2C32B1159800"
-        
-        let dataStore = backendless.data.of(Tool.ofClass())
-        
-        // save object synchronously
-        var error: Fault?
-        let result = dataStore.save(hammer, fault: &error) as? Tool
-        if error == nil {
-            print("Contact has been saved: \(result!.objectId)")
-        }
-        else {
-            print("Server reported an error: \(error)")
-        }
-        
-    }
+//    })
+//    }
+  
+//    func findContactByName() {
+//        
+//        let whereClause = "title = 'hammer'"
+//        let dataQuery = BackendlessDataQuery()
+//        dataQuery.whereClause = whereClause
+//        
+//        var error: Fault?
+//        let bc = backendless.data.of(Users.ofClass()).find(dataQuery, fault: &error)
+//        if error == nil {
+//            print("Contacts have been found: \(bc.data)")
+//        }
+//        else {
+//            print("Server reported an error: \(error)")
+//        }
+//    }
 
 }
 
