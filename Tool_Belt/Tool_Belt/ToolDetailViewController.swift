0282d43502137873caa89732d3034c6d737b08bd
Emmet Susslin <esusslin@gmail.com>
2016-09-21 21:42:00 -0700

    tool detail recovery method pending

--- Tool_Belt/Tool_Belt/ToolDetailViewController.swift
+++ Tool_Belt/Tool_Belt/ToolDetailViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import Mapbox
 
 class ToolDetailViewController: UIViewController {
     
+    @IBOutlet weak var toolImageView: UIImageView!
+    @IBOutlet weak var toolTitle: UILabel!
+    @IBOutlet weak var toolMake: UILabel!
+    @IBOutlet weak var toolDescription: UILabel!
     
+
     var toolId: String?
     var ownerId: String?
+    var tool: Tool?
+    var owner: BackendlessUser?
     
 
     override func viewDidLoad() {
@@ -20,13 +28,43 @@ class ToolDetailViewController: UIViewController {
         
         let backButton = UIBarButtonItem(title: "", style:.Plain, target: nil, action: nil)
         self.navigationItem.backBarButtonItem = backButton
-
         
+        
+        getTool()
+        
+//        getOwner()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
        
+    }
+    
+    func getTool() {
+        
+        let whereClause = "objectId = '\(self.toolId)'"
+        let dataQuery = BackendlessDataQuery()
+        
+        dataQuery.whereClause = whereClause
+        var error: Fault?
+        
+        
+        let tools = Backendless.sharedInstance().data.of(Tool.ofClass())
+        
+        tools.find(dataQuery, response: { (tools : BackendlessCollection!) in
+        
+        if let tool = tools.data.first as? Tool {
+            print("********")
+            print(tool.title)
+            print("********")
+            self.tool = tool
+            print("********")
+        }
+        
+       }) { (fault : Fault!) -> Void in
+        print("Server report an error : \(fault)")
+        }
+
     }
     
 
